bfb43b302b762da87ceb40278d2de76f5d5b217a
entropybits <adrian@entropybits.com>
2013-06-07 11:36:42 -0400

    Implemented tour function.

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 whats-up-voyager-html
 =====================
+An interactive tour of the Voyager 1 mission.
+
+Requirements:
+- Google Earth browser plugin
+- Optional: LeapMotion controller and driver; development release only as of June 2013
+
+To run: 
+- Open voyager-tour.html in browser of choice. Click the buttons in sequence to see specific points in Voyager's journey. The tour buttons flies along Voyager's coordinates from 06 Sept 1977 to 31 Dec 2020.
+
+Known bugs:
+- If the launch button is clicked out of sequence, the launch does not start from origin.
+- If the day buttons are clicked out of sequence, the view does not always immediately switch to the correct coordinates.
+- Timeline and historical imagery do not work in Sky mode for the Voyager path. Does the imagery exist in the Google database?
+
 Team Members
 
 Adrian Nikolica
@@ -12,4 +26,3 @@ Craig Greenburg
 
 Yi Wang
 
-*NOTE: The voyager_day2.kml file needs it's absolute path. There is a clunky way to do this in Javascript but it feels wrong. We should probably get it hosted.
--- utilities/convert_plaintext_2_kml.py
+++ utilities/convert_plaintext_2_kml.py
@@ -1,5 +1,5 @@
 
-def skiphead(a_file, breakstring='$$SOE'):
+def skiphead(a_file, breakstring='Date__(UT)_	R.A._(J2000_	DEC.)'):
     #
     #
     for line in a_file:
@@ -9,7 +9,18 @@ def skiphead(a_file, breakstring='$$SOE'):
         if line == breakstring:
             return
 
-def read_ra_dec(filepath, breakstring='$$EOE'):
+def convert_date(date):
+    # Convert stripped date from "YYYY-[month name]-DD" to "YYYY-MM-DD"
+    months = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06',
+              'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
+    month = date[5:8]
+    if month in months.keys():
+        new_date = date[0:5] + months[month] + date[8:]
+    else:
+        new_date = ''
+    return new_date
+
+def read_ra_dec(filepath, breakstring='\t'):
     #
     #
     ra_dec = []
@@ -22,28 +33,37 @@ def read_ra_dec(filepath, breakstring='$$EOE'):
             if line == breakstring:
                 break
             dd = line.split()
-            a_date, a_time, ra, dec = line.split()
-            ra_dec.append((ra, dec))
+            a_date, ra, dec = line.split()
+            date = convert_date(a_date)
+            ra_dec.append((date, ra, dec))
     return ra_dec
 
 def write_ra_dec(filepath, ra_decs):
-    head = """
-<?xml version="1.0" encoding="UTF-8"?>
+    head = """<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2"
   xmlns:gx="http://www.google.com/kml/ext/2.2">
   
   <gx:Tour>
-    <name>Day 1 and 60</name>
+    <name>Voyager 1 Tour, 1977-Sep-06 to 2020-Dec-31 </name>
     <gx:Playlist>
 """
 
     with open(filepath, 'w') as outfile:
         outfile.write(head)
-        for ra, dec in ra_decs:
+        for date, ra, dec in ra_decs:
+#            if date == "1979-05-05": # Stop file at this date
+#                tail = """    </gx:Playlist>
+#  </gx:Tour>
+#</kml>"""
+#                outfile.write(tail)
+#                return
             point = """<gx:FlyTo>
         <gx:duration>4.0</gx:duration>
         <gx:flyToMode>smooth</gx:flyToMode>
         <LookAt>
+          <gx:TimeStamp>
+           <when>%s</when>
+          </gx:TimeStamp>
           <longitude>%s</longitude>
           <latitude>%s</latitude>
           <altitude>0</altitude>
@@ -52,7 +72,7 @@ def write_ra_dec(filepath, ra_decs):
           <range>162401</range>
           <altitudeMode>relativeToGround</altitudeMode>
         </LookAt>
-      </gx:FlyTo>"""%(ra, dec)
+      </gx:FlyTo>"""%(date, ra, dec)
             outfile.write(point+'\n')
     
         tail = """</gx:Playlist>
@@ -67,6 +87,6 @@ def main(in_filepath, out_filepath):
 if __name__ == '__main__':
     import os
     dir = os.path.dirname(__file__)
-    in_filepath = os.path.join(dir, 'horizons_results.txt')
+    in_filepath = os.path.join(dir, 'voyager_coordinates.txt')
     out_filepath = os.path.join(dir, 'voyager_coordinates.kml')
     main(in_filepath, out_filepath)
